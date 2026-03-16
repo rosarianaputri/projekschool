aa3dc1b6ff1989a2219139c6e20ceff193cb315d
rosarianaputri <putrirosariana@gmail.com>
2026-03-16 14:56:13 +0700

    ubah ukuran logo navbar

--- resources/views/layouts/partials/frontend-navbar.blade.php
+++ resources/views/layouts/partials/frontend-navbar.blade.php
@@ -1,56 +1,86 @@
-<nav class="navbar navbar-expand-lg navbar-light py-0" style="background: white !important;">
+<style>
+    .navbar{
+        height:70px;
+        padding-top:0;
+        padding-bottom:0;
+        background:white !important;
+    }
+
+    .logo-navbar{
+        height:110px; 
+        width:auto;
+        object-fit:contain;
+    }
+
+    .navbar-brand{
+        display:flex;
+        align-items:center;
+        height:70px;
+        overflow:hidden;
+    }
+
+    .navbar-nav .nav-link{
+        font-weight:500;
+        padding-left:15px;
+        padding-right:15px;
+    }
+</style>
+
+<nav class="navbar navbar-expand-lg navbar-light">
     <div class="container">
-        @php
-            $logoPath = \App\Models\SiteSetting::getValue('school_logo');
-            $logoUrl = $logoPath ? asset($logoPath) : asset('images/default-logo.png');
-        @endphp
 
-        <a class="navbar-brand d-flex align-items-center" href="{{ route('front.home') }}">
-            <img src="{{ $logoUrl }}" 
-                 alt="Layla School"
-                 style="height:100px; width:auto;">
-        </a>
+    @php
+        $logoPath = \App\Models\SiteSetting::getValue('school_logo');
+        $logoUrl = $logoPath ? asset($logoPath) : asset('images/default-logo.png');
+    @endphp
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    <a class="navbar-brand" href="{{ route('front.home') }}">
+        <img src="{{ $logoUrl }}" alt="Layla School" class="logo-navbar">
+    </a>
 
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto align-items-center">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('front.home') ? 'active' : '' }}" href="{{ route('front.home') }}">Home</a>
-                </li>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto align-items-center">
 
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('front.about') ? 'active' : '' }}" href="{{ route('front.about') }}">About Us</a>
-                </li>
+            <li class="nav-item">
+                <a class="nav-link {{ request()->routeIs('front.home') ? 'active' : '' }}" href="{{ route('front.home') }}">Home</a>
+            </li>
 
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('front.academic') ? 'active' : '' }}" href="{{ route('front.academic') }}">Academic</a>
-                </li>
+            <li class="nav-item">
+                <a class="nav-link {{ request()->routeIs('front.about') ? 'active' : '' }}" href="{{ route('front.about') }}">About Us</a>
+            </li>
 
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('front.facilities') ? 'active' : '' }}" href="{{ route('front.facilities') }}">Facilities</a>
-                </li>
+            <li class="nav-item">
+                <a class="nav-link {{ request()->routeIs('front.academic') ? 'active' : '' }}" href="{{ route('front.academic') }}">Academic</a>
+            </li>
 
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('front.student_life') ? 'active' : '' }}" href="{{ route('front.student_life') }}">Student Life</a>
-                </li>
+            <li class="nav-item">
+                <a class="nav-link {{ request()->routeIs('front.facilities') ? 'active' : '' }}" href="{{ route('front.facilities') }}">Facilities</a>
+            </li>
 
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('front.information') ? 'active' : '' }}" href="{{ route('front.information') }}">Information</a>
-                </li>
+            <li class="nav-item">
+                <a class="nav-link {{ request()->routeIs('front.student_life') ? 'active' : '' }}" href="{{ route('front.student_life') }}">Student Life</a>
+            </li>
 
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('front.ppdb') || request()->routeIs('front.ppdb.form') ? 'active' : '' }}" href="{{ route('front.ppdb') }}">PPDB</a>
-                </li>
+            <li class="nav-item">
+                <a class="nav-link {{ request()->routeIs('front.information') ? 'active' : '' }}" href="{{ route('front.information') }}">Information</a>
+            </li>
 
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('front.contact') ? 'active' : '' }}" href="{{ route('front.contact') }}">Contact Us</a>
-                </li>
+            <li class="nav-item">
+                <a class="nav-link {{ request()->routeIs('front.ppdb') || request()->routeIs('front.ppdb.form') ? 'active' : '' }}" href="{{ route('front.ppdb') }}">PPDB</a>
+            </li>
 
-            </ul>
-        </div>
+            <li class="nav-item">
+                <a class="nav-link {{ request()->routeIs('front.contact') ? 'active' : '' }}" href="{{ route('front.contact') }}">Contact Us</a>
+            </li>
+
+        </ul>
     </div>
+
+</div>
+
+
 </nav>
